d915ea94c1768f3b6273e895fcc4e3a81b71450d
David Forer <david.j.forer@gmail.com>
2019-02-21 16:16:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,7 +60,9 @@ def monopoly_with_fourth_tier
 	        reading_railroad: {
 	          "mortgage_value" => "$100"
 	        },
-	        pennsylvania_railroad: {},
+	        pennsylvania_railroad: {
+	          
+	        },
 	        b_and_o_railroad: {},
 	        shortline: {}
 	      },
